377f918368f70a117993864b266f14958742ce6e
Gareth Ellis <gareth.ellis0@gmail.com>
2019-01-26 19:16:02 -0800

    Created Function To Get EDIDs

---
+++ include/utils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Screen.h>
+#include <vector>
+#include <string>
+
+namespace i3ScreenManager {
+    namespace utils {
+
+        /**
+         * Gets the EDID for every screen attached to this system
+         *
+         * @param display_name the name of the display to get the screens from
+         * (ex. ":0")
+         * @return all the EDID's for screens attached to this computer
+         */
+        std::vector<i3ScreenManager::EDID> getAllEDIDs(std::string display_name);
+    }
+};
