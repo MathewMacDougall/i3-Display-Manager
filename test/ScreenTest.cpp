a0be49867b4d0ef50646574f17de7c20cd00c838
Gareth Ellis <gareth.ellis0@gmail.com>
2019-01-26 16:30:51 -0800

    Added Basic Unit Testing

---
+++ test/ScreenTest.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "Screen.h"
+
+class ScreenTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+        // Code here will be called immediately after each test
+        // (right before the destructor).
+    }
+};
+
+TEST_F(ScreenTest, delete_me) {
+    Screen s;
+}
